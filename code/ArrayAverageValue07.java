d5f7b2721a41e59d5e1c95045782e6c56d37d794
azariacindy <cindysss099@gmail.com>
2023-11-05 21:52:35 +0700

    add new file

--- code/ArrayAverageValue07.java
+++ code/ArrayAverageValue07.java
@@ -4,21 +4,47 @@ public class ArrayAverageValue07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] studGrade = new int[10];
+        System.out.print("Enter the number of students: ");
+        int numStudents = sc.nextInt();
+        int[] studGrade = new int[numStudents];
         int total = 0;
         double average;
+        int passCount = 0; // Initialize the number of students who passed
+        int passTotal = 0;
+        int failCount = 0; // Initialize the number of students who failed
+        int failTotal = 0;
 
         for (int i = 0; i < studGrade.length; i++) {
-            System.out.print("Enter student's grade " + (i + 1) + " : ");
+            System.out.print("Enter student " + (i + 1) + "'s grade: ");
             studGrade[i] = sc.nextInt();
-        }
 
-        for (int i = 0; i < studGrade.length; i++) {
             total += studGrade[i];
+
+            if (studGrade[i] > 70) {
+                passCount++;
+                passTotal += studGrade[i];
+            } else {
+                failCount++;
+                failTotal += studGrade[i];
+            }
         }
 
-        average = (double) total / studGrade.length;
-        System.out.println("Average value: " + average);
+        average = (double) total / numStudents;
+        System.out.println("Average value for all students: " + average);
+
+        if (passCount > 0) {
+            double passAverage = (double) passTotal / passCount;
+            System.out.println("Average value for passing students: " + passAverage);
+        } else {
+            System.out.println("No students passed.");
+        }
+
+        if (failCount > 0) {
+            double failAverage = (double) failTotal / failCount;
+            System.out.println("Average value for failing students: " + failAverage);
+        } else {
+            System.out.println("No students failed.");
+        }
 
         sc.close();
     }
