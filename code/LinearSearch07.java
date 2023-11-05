5ffcc72e2c7643a88fa96beb1f76ded236a20c1a
azariacindy <cindysss099@gmail.com>
2023-11-05 21:59:32 +0700

    add new file

---
+++ code/LinearSearch07.java
@@ -0,0 +1,17 @@
+public class LinearSearch07 {
+    public static void main (String[] args) {
+
+        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        int key = 20;
+        int result = 0;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key){
+                result = i;
+                break;
+            }
+        }
+        System.out.println("The key is in the array at index position : "+result);
+
+    }
+}
