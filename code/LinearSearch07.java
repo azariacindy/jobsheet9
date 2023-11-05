23535b5d5540ea1d04f47e7d7d0e2a64395a853d
azariacindy <cindysss099@gmail.com>
2023-11-05 22:14:14 +0700

    add new changes

--- code/LinearSearch07.java
+++ code/LinearSearch07.java
@@ -1,17 +1,37 @@
+import java.util.Scanner;
+
 public class LinearSearch07 {
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int result = 0;
+        System.out.print("Enter the number of elements in the array: ");
+        int numElements = sc.nextInt();
 
-        for (int i = 0; i < arrayInt.length; i++) {
+        int[] arrayInt = new int[numElements];
+
+        for (int i = 0; i < numElements; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the key to search for: ");
+        int key = sc.nextInt();
+
+        int result = -1; // Initialize result to -1 (not found)
+
+        for (int i = 0; i < numElements; i++) {
             if (arrayInt[i] == key){
                 result = i;
                 break;
             }
         }
-        System.out.println("The key is in the array at index position : "+result);
 
+        if (result != -1) {
+            System.out.println("The key is in the array at index position: " + result);
+        } else {
+            System.out.println("The key is not found in the array.");
+        }
+
+        sc.close();
     }
 }
